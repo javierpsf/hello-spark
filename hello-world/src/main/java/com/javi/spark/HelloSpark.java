ab76f1072263a821d6f0958f3e139bf625272a3f
javierpsf <javierpsf@gmail.com>
2016-03-05 13:43:03 +0100

    Pruebas de concepto

--- hello-world/src/main/java/com/javi/spark/HelloSpark.java
+++ hello-world/src/main/java/com/javi/spark/HelloSpark.java
@@ -1,12 +1,54 @@
 package com.javi.spark;
 
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import com.javi.spark.model.Book;
 
 public class HelloSpark {
 
+	private static Map<String, Book> books = new HashMap<String, Book>();
+	
 	public static void main(String[] args) {
+		
+		final Random random = new Random();
+		
 		get("/hello", (request, response) -> "Hello Javi!");
+		
+		
+        get("/users/:name", (request, response) -> "Selected user: " + request.params(":name"));
+        
+        post("/books", (request, response) -> {
+            String author = request.queryParams("author");
+            String title = request.queryParams("title");
+            Book book = new Book(author, title);
+
+            int id = random.nextInt(Integer.MAX_VALUE);
+            books.put(String.valueOf(id), book);
+
+            response.status(201); // 201 Created
+            return id;
+        });
+        
+        
+        get("/books/:id", (request, response) -> {
+            Book book = books.get(request.params(":id"));
+            Boolean book2 = books.containsValue("javi");
+            if (book != null) {
+                return "Title: " + book.getTitle() + ", Author: " + book.getAuthor();
+            } else {
+                response.status(404); // 404 Not found
+                return "Book not found";
+            }
+        });
+
+
+		
 	}
 
 }
---
+++ hello-world/src/main/java/com/javi/spark/model/Book.java
@@ -0,0 +1,28 @@
+package com.javi.spark.model;
+
+public class Book {
+	
+	public String author, title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
